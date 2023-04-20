b9988172e835ce5003da27e1d284bc95f3d91cbb
Uyên Tôn Võ <tonuyen2002@gmail.com>
2023-04-20 12:54:45 +0700

    Create Rectangle.java

---
+++ project/Rectangle.java
@@ -0,0 +1,32 @@
+/*Game: Battleship
+  Rectangle Class:
+  A simple Rectangle with a position defined by the top left corner,
+  and a width,height to represent the size of the Rectangle
+ */ 
+
+public class Rectangle {
+    protected Position position;
+    protected int width;
+    protected int height;
+
+    public Rectangle(int x, int y, int width, int height) {
+        this(new Position(x,y), width, height);
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    /*Test if the targetPosition is inside the Rectangle*/
+    public boolean isPositionInside(Position targetPosition) {
+        return targetPosition.x >= position.x && targetPosition.y >= position.y && targetPosition.x < position.x + width && targetPosition.y < position.y + height;
+    }
+}
